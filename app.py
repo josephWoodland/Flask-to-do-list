d546666e9680734987f114489d2bc3cc83833360
Joseph Woodland <joseph@workit-app.com>
2021-08-15 18:34:54 +0100

    Added password verification and the users profil page

--- app.py
+++ app.py
@@ -48,6 +48,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -62,7 +64,9 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session['user'] = request.form.get("username").lower()
-                    flash('Welcome, {}'.format(request.form.get("username").capitalize()))
+                    flash('Welcome, {}'.format(
+                        request.form.get("username").capitalize()))
+                    return redirect(url_for("profile", username=session["user"]))
             else:
                 # invalid password match
                 flash('Incorrect Username and/or Password')
@@ -72,8 +76,16 @@ def login():
             # username does not exist
             flash('Incorrect Username and/or Password')
             return redirect(url_for('login'))
+            
 
     return render_template("login.html")
+
+
+@app.route('/profile/<username>', methods=["GET","POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one({"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
